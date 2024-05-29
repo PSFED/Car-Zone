706dbe57f8ac968d1bb03305985ca883fa2bcdde
Pavel Semenov <x.pavel.semenov.x@gmail.com>
2024-05-29 12:35:07 +0300

    creating navbar links and pages, added static files

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
-from .views import home
+from .views import home, about, services, contact
 
 
 urlpatterns = [
     path('', home, name='home'),
+    path('about', about, name='about'),
+    path('services', services, name='services'),
+    path('contact', contact, name='contact'),
 ]
