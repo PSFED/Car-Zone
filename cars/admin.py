6b44a8117b116451083984198db0db45c8875147
Pavel Semenov <x.pavel.semenov.x@gmail.com>
2024-06-02 16:00:14 +0300

    upgraded model and binded with templates

--- cars/admin.py
+++ cars/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
-
 from cars.models import Car
+from django.utils.html import format_html
 
-admin.site.register(Car)
+
+class CarAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="40" style="border-radius: 50px;" />'.format(object.car_photo.url))
+
+    thumbnail.short_description = "Car image"
+    list_display = ('id', 'thumbnail', 'car_title', 'city', 'color', 'model', 'year',
+                    'body_style', 'fuel_type', 'is_featured')
+    list_display_links = ('thumbnail', 'car_title',)
+    list_editable = ('is_featured',)
+    search_fields = ('id', 'car_title', 'city', 'model',
+                     'body_style', 'fuel_type',)
+    list_filter = ('city', 'model', 'body_style', 'fuel_type',)
+
+
+admin.site.register(Car, CarAdmin)
